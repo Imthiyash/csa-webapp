48d07e499e9f2e8fcca1ae1605d20fc5330499f4
Imthiyash <skimthiyash7257@gmail.com>
2025-03-27 11:51:37 +0530

    Added_announcements

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-    before_action :authenticate_user!, only: :profile
+    before_action :authenticate_user!, except: :home
 
     def home
       # @announcements = Announcement.all
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_27_045132) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_27_062026) do
+  create_table "announcements", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "batch_points", force: :cascade do |t|
     t.string "batch_name"
     t.decimal "batch_points"
